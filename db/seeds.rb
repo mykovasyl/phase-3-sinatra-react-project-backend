9034d77af178f01ee700bf737dcbc87af84809af
Mykola Vasyl'yev <mykovasyl@gmail.com>
2022-08-23 11:56:46 -0400

    create additional seeds for chores

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,6 +11,48 @@ Chore.create(
   points: 10,
   due_by: "2022-08-27",
   child_id: 10,
+  completed: false
+)
+Chore.create(
+  name: "Do homework",
+  points: 25,
+  due_by: "2022-08-26",
+  child_id: 14,
+  completed: false
+)
+Chore.create(
+  name: "Wash dishes",
+  points: 5,
+  due_by: "2022-09-05",
+  child_id: 15,
+  completed: false
+)
+Chore.create(
+  name: "Take out trash",
+  points: 5,
+  due_by: "2023-08-27",
+  child_id: 16,
+  completed: false
+)
+Chore.create(
+  name: "Get the mail",
+  points: 15,
+  due_by: "2022-08-28",
+  child_id: 10,
+  completed: false
+)
+Chore.create(
+  name: "Mop the floors",
+  points: 15,
+  due_by: "2022-08-27",
+  child_id: 14,
+  completed: false
+)
+Chore.create(
+  name: "Mop the floors",
+  points: 10,
+  due_by: "2022-08-27",
+  child_id: 10,
   completed: true
 )
 Chore.create(
